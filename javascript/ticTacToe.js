9e5651dcfcfeba95f5c7412f4da230b6c45b6ec5
Skyler Edvik <edvik.skyler@gmail.com>
2021-09-09 14:15:48 -0600

    Complete displayController.init function and basic page styling

--- javascript/ticTacToe.js
+++ javascript/ticTacToe.js
@@ -1,25 +1,57 @@
 // gameBoard module - contains logic related to the game board
 const gameBoard = (function() {
     // state array - contains game board state
-    const _state = [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
+    let _state = [
+        ['X', 'X', 'O'],
+        ['O', 'O', 'X'],
+        ['X', 'O', 'X']
     ];
     function getState() {
         return _state;
     }
+    function resetState() {
+        _state = [
+            ['', '', ''],
+            ['', '', ''],
+            ['', '', '']
+        ];
+    }
+    function addSymbol(symbol, row, col) {
+
+    }
 
     return {
-        getState
+        getState,
+        resetState,
+        addSymbol
     };
 })();
 
 // displayController module - contains DOM manipulation logic
 const displayController = (function() {
+    // init function - sets up initial game board html
+    function init(gameState) {
+        const boardContainer = document.querySelector('#board-container');
+        // Add a board space div for all 9 board spaces
+        for (let row = 0; row < 3; row++) {
+            for (let col = 0; col < 3; col++) {
+                const boardSpace = document.createElement('div');
+                boardSpace.classList.add('board-space')
+                boardSpace.setAttribute('data-row', row);
+                boardSpace.setAttribute('data-col', col);
+                boardSpace.textContent = gameState[row][col];
+                boardContainer.appendChild(boardSpace);
+            }
+        }
+    }
+    
+    function render(gameState) {
+        // Loop through gameState array and populate DOM elements with appropriate symbol
+    }
 
     return {
-
+        init,
+        render
     };
 })();
 
@@ -41,8 +73,14 @@ const playerFactory = function(name, symbol) {
     function getSymbol() {
         return _symbol;
     }
+    function play() {
+
+    }
     return {
         getName,
         getSymbol
     };
 }
+
+// TEST CALLS
+displayController.init(gameBoard.getState());
